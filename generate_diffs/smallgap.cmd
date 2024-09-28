e514fa8061dd63e899ae14f78fffdd38a23094ad
invictus125 <50851487+invictus125@users.noreply.github.com>
2024-09-28 08:44:47 -0500

    Tweak smallgap validation script

--- generate_diffs/smallgap.cmd
+++ generate_diffs/smallgap.cmd
@@ -2,3 +2,6 @@ magick compare -fuzz 2% ../smallgap.png ../correct_outputs/rast-smallgap.png sma
 magick composite ../smallgap.png ../correct_outputs/rast-smallgap.png -alpha off -compose difference smallgap_rawdiff.png
 magick convert smallgap_rawdiff.png -level 0%,8% smallgap_diff.png
 magick convert +append ../correct_outputs/rast-smallgap.png ../smallgap.png smallgap_compare.png smallgap_rawdiff.png smallgap_diff.png ../smallgap_full_diff.png
+del smallgap_compare.png
+del smallgap_rawdiff.png
+del smallgap_diff.png
